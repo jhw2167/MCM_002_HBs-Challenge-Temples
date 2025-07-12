021afb765f16ef6a0c664b69e78b7d1add8204f8
jhw2167 <jhw2167@columbia.edu>
2025-07-12 15:53:56 -0500

    refactor: Convert ChallengeBed from RespawnAnchorBlock to basic Block with custom properties

--- common/src/main/java/com/holybuckets/challengetemple/block/ChallengeBed.java
+++ common/src/main/java/com/holybuckets/challengetemple/block/ChallengeBed.java
@@ -1,15 +1,17 @@
 package com.holybuckets.challengetemple.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RespawnAnchorBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
 
-public class ChallengeBed extends RespawnAnchorBlock {
+public class ChallengeBed extends Block {
     public ChallengeBed() {
-        super(Properties.copy(Blocks.RESPAWN_ANCHOR)
+        super(Properties.copy(Blocks.STONE_BRICKS)
             .destroyTime(-1f)
             .explosionResistance(3600000f)
             .pushReaction(PushReaction.IGNORE)
@@ -17,12 +19,7 @@ public class ChallengeBed extends RespawnAnchorBlock {
     }
 
     @Override
-    public boolean canBeCharged(BlockState state) {
-        return state.getValue(CHARGE) < 4;
-    }
-
-    @Override
-    public void animateTick(BlockState state, Level level, BlockPos pos, java.util.Random random) {
-        // Override to prevent particle effects if desired
+    public int getLightBlock(BlockState $$0, BlockGetter $$1, BlockPos $$2) {
+        return 0;
     }
 }

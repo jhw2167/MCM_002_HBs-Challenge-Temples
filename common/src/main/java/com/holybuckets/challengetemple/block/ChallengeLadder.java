7ca362d6e415c01898c9a86c77b3176ca60ecbe7
jhw2167 <jhw2167@columbia.edu>
2025-07-14 17:51:16 -0500

    refactor: Simplify ChallengeLadder block properties using Blocks.LADDER copy

--- common/src/main/java/com/holybuckets/challengetemple/block/ChallengeLadder.java
+++ common/src/main/java/com/holybuckets/challengetemple/block/ChallengeLadder.java
@@ -1,13 +1,13 @@
 package com.holybuckets.challengetemple.block;
 
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LadderBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 
 public class ChallengeLadder extends LadderBlock {
     public ChallengeLadder() {
-        super(BlockBehaviour.Properties.of(Material.DECORATION)
+        super(Properties.copy(Blocks.LADDER)
                 .strength(ModBlocks.CHALLENGE_BLOCK_STRENGTH)
                 .explosionResistance(ModBlocks.CHALLENGE_BLOCK_EXPL_RES)
                 .sound(SoundType.LADDER)

ab42c5f0f3b45b71d7d8a82a1c24ff97c0743dc8
jhw2167 <jhw2167@columbia.edu>
2025-07-17 15:09:50 -0500

    refactor: Simplify block registration syntax and uncomment previously commented blocks

--- common/src/main/java/com/holybuckets/challengetemple/block/ModBlocks.java
+++ common/src/main/java/com/holybuckets/challengetemple/block/ModBlocks.java
@@ -73,50 +73,31 @@ public class ModBlocks {
         blocks.register(() -> challengeGlass = new ChallengeGlass(), () -> itemBlock(challengeGlass), id("challenge_glass"));
         blocks.register(() -> challengePushableGlass = new ChallengePushableGlass(), () -> itemBlock(challengePushableGlass), id("challenge_pushable_glass"));
         blocks.register(() -> challengeGlassPane = new ChallengeGlassPane(), () -> itemBlock(challengeGlassPane), id("challenge_glass_pane"));
-        //blocks.register(() -> challengeWood = new ChallengeWood(), () -> itemBlock(challengeWood), id("challenge_wood"));
 
         blocks.register(() -> challengeFauxBrick = new ChallengeFauxBrick(), () -> itemBlock(challengeFauxBrick), id("challenge_faux_brick"));
         blocks.register(() -> challengePushableFauxBrick = new ChallengePushableFauxBrick(), () -> itemBlock(challengePushableFauxBrick), id("challenge_pushable_faux_brick"));
         blocks.register(() -> challengeInvisibleBrick = new ChallengeInvisibleBrick(), () -> itemBlock(challengeInvisibleBrick), id("challenge_invisible_brick"));
         blocks.register(() -> challengePushableInvisibleBrick = new ChallengePushableInvisibleBrick(), () -> itemBlock(challengePushableInvisibleBrick), id("challenge_pushable_invisible_brick"));
+        blocks.register(() -> challengeStairs = new ChallengeStairs(), () -> itemBlock(challengeStairs), id("challenge_stairs"));
+        blocks.register(() -> challengePushableStairs = new ChallengePushableStairs(), () -> itemBlock(challengePushableStairs), id("challenge_pushable_stairs"));
 
         blocks.register(() -> challengeStone = new ChallengeStone(), () -> itemBlock(challengeStone), id("challenge_stone"));
         blocks.register(() -> challengeCobble = new ChallengeCobble(), () -> itemBlock(challengeCobble), id("challenge_cobble"));
         blocks.register(() -> challengeLog = new ChallengeLog(), () -> itemBlock(challengeLog), id("challenge_log"));
+        blocks.register(() -> challengeWood = new ChallengeWood(), () -> itemBlock(challengeWood), id("challenge_wood"));
 
         //DeferredObject<Block> registerBlock(Function<ResourceLocation, Block> supplier, ResourceLocation identifier);
         //challengeChest = blocks.registerBlock( loc -> new ChallengeChestBlock()  , id("challenge_chest"));
-        blocks.register(() -> challengeChest = new ChallengeChestBlock(),
-            () -> new ChallengeChestItem(challengeChest, new Properties()),
-            id("challenge_chest"));
+        blocks.register(() -> challengeChest = new ChallengeChestBlock(), () -> new ChallengeChestItem(challengeChest, new Properties()), id("challenge_chest"));
+        blocks.register(() -> challengeLadder = new ChallengeLadder(), () -> itemBlock(challengeLadder), id("challenge_ladder"));
             
-        blocks.register(() -> challengeLadder = new ChallengeLadder(),
-            () -> itemBlock(challengeLadder),
-            id("challenge_ladder"));
-            
-        blocks.register(() -> challengeDoor = new ChallengeDoor(),
-            () -> itemBlock(challengeDoor),
-            id("challenge_door"));
-            
-        blocks.register(() -> challengeGoldPlate = new ChallengePressurePlates.ChallengeLightPlate(),
-            () -> itemBlock(challengeGoldPlate),
-            id("challenge_gold_plate"));
-            
-        blocks.register(() -> challengeClearingPlate = new ChallengePressurePlates.ChallengeClearingPlate(),
-            () -> itemBlock(challengeClearingPlate),
-            id("challenge_clearing_plate"));
-            
-        blocks.register(() -> challengeStonePlate = new ChallengePressurePlates.ChallengePressurePlate(),
-            () -> itemBlock(challengeStonePlate),
-            id("challenge_stone_plate"));
-            
-        blocks.register(() -> challengeButton = new ChallengeButton(),
-            () -> itemBlock(challengeButton),
-            id("challenge_button"));
-            
-        blocks.register(() -> challengeLever = new ChallengeLever(),
-            () -> itemBlock(challengeLever),
-            id("challenge_lever"));
+        blocks.register(() -> challengeDoor = new ChallengeDoor(), () -> itemBlock(challengeDoor), id("challenge_door"));
+        blocks.register(() -> challengeGoldPlate = new ChallengePressurePlates.ChallengeLightPlate(), () -> itemBlock(challengeGoldPlate), id("challenge_gold_plate"));
+        blocks.register(() -> challengeClearingPlate = new ChallengePressurePlates.ChallengeClearingPlate(), () -> itemBlock(challengeClearingPlate), id("challenge_clearing_plate"));
+
+        blocks.register(() -> challengeStonePlate = new ChallengePressurePlates.ChallengePressurePlate(), () -> itemBlock(challengeStonePlate), id("challenge_stone_plate"));
+        blocks.register(() -> challengeButton = new ChallengeButton(), () -> itemBlock(challengeButton), id("challenge_button"));
+        blocks.register(() -> challengeLever = new ChallengeLever(), () -> itemBlock(challengeLever), id("challenge_lever"));
             
         blocks.register(() -> challengeLava = new ChallengeLava(), // TODO: Add proper fluid registration
             () -> itemBlock(challengeLava),
@@ -125,22 +106,9 @@ public class ModBlocks {
         blocks.register(() -> challengeTrapdoor = new ChallengeTrapdoor(),
             () -> itemBlock(challengeTrapdoor),
             id("challenge_trapdoor"));
-            
-        blocks.register(() -> challengeStairs = new ChallengeStairs(),
-            () -> itemBlock(challengeStairs),
-            id("challenge_stairs"));
-            
-        blocks.register(() -> challengePushableStairs = new ChallengePushableStairs(),
-            () -> itemBlock(challengePushableStairs),
-            id("challenge_pushable_stairs"));
-            
-        blocks.register(() -> challengeFence = new ChallengeFence(),
-            () -> itemBlock(challengeFence),
-            id("challenge_fence"));
-            
-        blocks.register(() -> challengeFenceGate = new ChallengeFenceGate(),
-            () -> itemBlock(challengeFenceGate),
-            id("challenge_fence_gate"));
+
+        blocks.register(() -> challengeFence = new ChallengeFence(), () -> itemBlock(challengeFence), id("challenge_fence"));
+        blocks.register(() -> challengeFenceGate = new ChallengeFenceGate(), () -> itemBlock(challengeFenceGate), id("challenge_fence_gate"));
 
         // Register building blocks for each color
 
